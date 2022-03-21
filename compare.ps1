2429e5ae0debc54421ad3f7bbfd162ad3c5e3259
Brandon Patterson <lilamar31@gmail.com>
2022-03-21 11:19:20 -0400

    added folder to git

---
+++ compare.ps1
@@ -0,0 +1,38 @@
+
+Write-Output $env:PATH
+
+# creates the employees csv 
+(Get-Content -Path /Users/brandonpatterson/Desktop/Jumpcloud/employees.json | ConvertFrom-Json)  | ConvertTo-Csv -NoTypeInformation | % { $_ -replace '"', "" } | Set-Content -Path /Users/brandonpatterson/Desktop/Jumpcloud/employees.csv
+
+# import gengineers 
+
+$engineers = Import-Csv -Path /Users/brandonpatterson/Desktop/Jumpcloud/engineering.csv  | Select-Object -Property firstName , lastName, manager, email  
+
+
+# import employees 
+$employees = Import-Csv -Path /Users/brandonpatterson/Desktop/Jumpcloud/employees.csv | Where-Object { $_.totp_enabled -eq 'false' } 
+$list = @()
+
+foreach ($f in $engineers) {
+    $matched = $false
+    foreach ($e in $employees) {
+        
+        if ($f.email -eq $e.email) {
+              
+              $new = [PSCustomObject]@{
+                'FirstName' = $e.firstName
+                'lastName' = $e.lastName
+                'email' = $e.email
+                'username' = $e.username
+                'JobTile'  = $e.JobTitle
+                'manager'  = $f.manager
+            }
+    
+            $list += $new
+        
+        }
+    }
+          
+}      
+    $List |select firstName , lastName , email , username , jobTile , manager | Export-Csv  -NoTypeInformation   -Path /Users/brandonpatterson/Desktop/Jumpcloud/list.csv   
+
